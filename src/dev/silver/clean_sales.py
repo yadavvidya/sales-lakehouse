3052582df2d2a4f8240ddd210c43256cf8090488
yadavvidya <yadavvidya23@gmail.com>
2026-01-20 17:34:28 +0530

    Added watermark, lookback window and permissions

--- src/dev/silver/clean_sales.py
+++ src/dev/silver/clean_sales.py
@@ -1,5 +1,5 @@
 import dlt
-from pyspark.sql.functions import current_timestamp, col
+from pyspark.sql.functions import current_timestamp, col, window, expr
 
 @dlt.table(
     name="sales_clean",
@@ -9,7 +9,13 @@ from pyspark.sql.functions import current_timestamp, col
 def sales_clean():
     return (
         dlt.read_stream("sales_raw_v2")
+        .option("watermark", "ingest_time", "10 minutes")
+        .groupBy(window(col("ingest_time"), "5 minutes", "10 minutes", "5 minutes"), col("country"))
         .withColumn("amount", col("amount").cast("double"))
+        .withColumn("country", col("country").cast("string"))
         .withColumn("ingest_time", col("ingest_time").cast("date"))
+        .filter(
+            col("order_time") >= current_timestamp() - expr("INTERVAL 2 DAYS")
+                )
     )
 
